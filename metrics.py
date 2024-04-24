f026b0dce7f767e8e8ce6b25422e4e9df2bfc03d
CommanderRedYT <ccommanderred@gmail.com>
2024-04-24 19:22:59 +0200

    Switch to args instead of env; Allow multiple devices

--- metrics.py
+++ metrics.py
@@ -5,61 +5,111 @@ from os import getenv
 from time import sleep
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
 from prometheus_client import start_http_server
+from urllib.parse import urlparse, urlunparse
+import string
+import argparse
 
+parser  = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--device',
+    action='append',
+    help='Can be used multiple times. DEVICE has format of "<ip>:<port> [user password]"',
+    nargs='+',
+    required=True
+)
+
+args = parser.parse_args()
+
+def argToConf(arg) -> dict:
+    if not isinstance(arg, list):
+        raise RuntimeError("Argument is not a list")
+    
+    if len(arg) == 1:
+        return (arg[0], None, None)
+    elif len(arg) == 3:
+        return (arg[0], arg[1], arg[2])
+    
+    raise RuntimeError("Wrong number of items for arg")
+
+devices = [argToConf(conf) for conf in args.device]
+
+printable = set(string.printable)
+ext_printable = set(string.printable + '°')
 
 class TasmotaCollector(object):
     def __init__(self):
-        self.ip = getenv('DEVICE_IP')
-        if not self.ip:
-            self.ip = "192.168.4.1"
-        self.user = getenv('USER')
-        self.password = getenv('PASSWORD')
+        self.devices = devices
 
     def collect(self):
+        for device in devices:
+            response = self.fetch(device[0], device[1], device[2])
 
-        response = self.fetch()
-
-        for key in response:
-            metric_name = "tasmota_" + key.lower().replace(" ", "_") 
-            metric = response[key].split()[0]
-            unit = None
-            if len(response[key].split()) > 1:
-                unit = response[key].split()[1]
-
-            if "today" in metric_name or "yesterday" in metric_name or "total" in metric_name:
-                r = CounterMetricFamily(metric_name, key, labels=['device'], unit=unit)
-            else:
-                r = GaugeMetricFamily(metric_name, key, labels=['device'], unit=unit)
-            r.add_metric([self.ip], metric)
-            yield r
-
-    def fetch(self):
-
-        url = 'http://' + self.ip + '/?m=1'
-
-        session = requests.Session()
-        
-        if self.user and self.password:
-            session.auth = (self.user, self.password)
-
-        page = session.get(url)
-
-        values = {}
-
-
-        string_values = str(page.text).split("{s}")
-        for i in range(1,len(string_values)):
-            try:
-                label = string_values[i].split("{m}")[0]
-                value = string_values[i].split("{m}")[1].split("{e}")[0]
-                if "<td" in value:
-                    value = value.replace("</td><td style='text-align:left'>", "")
-                    value = value.replace("</td><td>&nbsp;</td><td>", "")
-
-                values[label] = value
-            except IndexError:
+            if not response:
                 continue
-        return values
+
+            for key in response:
+                [value, unit] = response[key]
+                metric_name = "tasmota_" + key.lower().replace(" ", "_") 
+                metric = value.split()[0]
+
+                extra_labels = ['device']
+                extra_metrics = [device[0]]
+
+                if unit:
+                    extra_labels.append('unit')
+                    extra_metrics.append(unit)
+
+                if "today" in metric_name or "yesterday" in metric_name or "total" in metric_name:
+                    r = CounterMetricFamily(metric_name, key, labels=extra_labels)
+                else:
+                    r = GaugeMetricFamily(metric_name, key, labels=extra_labels)
+                r.add_metric(extra_metrics, metric)
+                yield r
+
+    def fetch(self, url: str, username: str | None = None, password: str | None = None, use_ssl: bool = False) -> dict | None:
+        try:
+            url = f'{"https" if use_ssl else "http"}://{url}/?m=1'
+
+            session = requests.Session()
+            
+            if username and password:
+                session.auth = (username, password)
+
+            response = session.get(url)
+            response.raise_for_status()
+
+            values = {}
+
+            string_values = str(response.text).split("{s}")
+            for i in range(1,len(string_values)):
+                try:
+                    label = string_values[i].split("{m}")[0]
+                    value = string_values[i].split("{m}")[1].split("{e}")[0]
+                    if "<td" in value:
+                        value = value.replace("</td><td style='text-align:left'>", "")
+                        value = value.replace("</td><td>&nbsp;</td><td>", "")
+
+                    label = label.strip()
+
+                    value = (''.join(filter(lambda x: x in ext_printable, value)))
+
+                    value = value.strip()
+
+                    unit = None
+
+                    if len(value.split()) > 1:
+                        unit = value.split()[1]
+                    elif 'Power Factor' in label:
+                        unit = ''
+
+                    values[label] = (value, unit)
+                except IndexError:
+                    continue
+            return values
+        except Exception as e:
+            print(e)
+            return {}
 
 def signal_handler(signal, frame):
     sys.exit(0)
@@ -68,9 +118,7 @@ signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == '__main__':
 
-    port = getenv('EXPORTER_PORT')
-    if not port:
-        port = 8000
+    port = getenv('EXPORTER_PORT', 8000)
 
     start_http_server(int(port))
     REGISTRY.register(TasmotaCollector())
